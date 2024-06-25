0e5a4e2840789049fe564842e30b5a32fcfaac7c
Prashant Purmah <prashant.purmah@sdworx.com>
2024-06-25 11:16:29 +0400

    Lecture 6

--- src/app/components/generate-code/generate-code.component.ts
+++ src/app/components/generate-code/generate-code.component.ts
@@ -1,21 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-generate-code',
   templateUrl: './generate-code.component.html',
   styleUrls: ['./generate-code.component.scss'],
 })
-export class GenerateCodeComponent  implements OnInit {
+export class GenerateCodeComponent  implements OnInit, OnChanges, OnDestroy {
 
   public code: string = '000000';
+  public isButtonEnabled = true;
+  public buttonLabel = 'Generate';
 
-  constructor() { }
+  constructor() {
+    console.log('constructor');
+   }
 
-  ngOnInit() {}
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
 
-  public generateCode() {
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges');
+  }
+
+  public generateButtonClicked() {
     console.log('Button clicked: ' + this.code);
-    const newCode = '123456';
-    this.code = newCode;
+    this.isButtonEnabled = false;
+    const newCode = this.generateRandomCode();
+    this.code = `${newCode}`;
+
+    let count = 5;
+
+    const intervalId = setInterval(() => {
+      this.buttonLabel = `${count}`;
+      if (count === 1) {
+        this.isButtonEnabled = true;
+        this.buttonLabel = 'Generate';
+        clearInterval(intervalId);
+      }
+      count--;
+    }, 1000);
+  }
+
+  private generateRandomCode() : number {
+    const min = 100000;
+    const max = 999999;
+
+    return Math.floor(Math.random()*(max-min + 1) + min);
   }
 }
--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  public email = 'ppp@gmail.com';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toggleChanged() : void {
+    console.log(this.email);
   }
 
 }
